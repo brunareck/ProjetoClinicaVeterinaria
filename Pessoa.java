7eca7361a0edf0d64b53899a6985eaa3e382e012
brunacreck <bruna.cr65@gmail.com>
2025-06-20 22:54:05 -0300

    Criada a superclasse Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,32 @@
+public abstract class Pessoa {
+
+    private String nome;
+
+    Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    Pessoa() {
+        
+    }   
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String mostrarDados() {
+        return "Nome: " + nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;   
+    }
+
+    public abstract String TipoPessoa();
+
+}
